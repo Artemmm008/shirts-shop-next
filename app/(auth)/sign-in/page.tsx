3921f15e195236379db45afa0e26174f8ac25150
Artemmm008 <artemshkola141@gmail.com>
2026-09-14 20:32:53 +0100

    updated sign-in page with force-dynamic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import css from "./signInPage.module.css";
 
+export const dynamic = "force-dynamic";
+
 export default function SignInPage() {
 	return (
 		<main className={css.page}>
